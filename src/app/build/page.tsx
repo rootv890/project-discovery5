81bef926c3ad19c6be73849fe6b4bb5cdb8370b3
prootv <prootv.31@gmail.com>
2025-06-15 12:05:47 +0530

    m3 typography

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -3,20 +3,37 @@ import { useState } from "react"
 import SearchIsland from "@/components/SearchIsland"
 import SearchTrigger from "@/components/SearchTrigger"
 import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { ArrowRight3 } from "iconsax-reactjs"
 
 const BuildPage = () => {
 	const [open, setOpen] = useState(false)
 
 	return (
 		<div className="flex flex-col items-center justify-center h-full w-full gap-2">
+			<div>
+				<p className="display-lg-em ">Build with Confidence</p>
+				<p className="body-md">Build with Confidence</p>
+			</div>
 			<Input
 				placeholder="Share your feedback..."
-				className="max-w-md"
+				className="max-w-md "
 			/>
 			<SearchTrigger
 				open={open}
 				setOpen={setOpen}
 			/>
+			<Button
+				variant={"inverted"}
+				size={"sm"}
+				className="mt-6"
+			>
+				Build with Confidence
+				<ArrowRight3
+					className="size-7"
+					variant="Bulk"
+				/>
+			</Button>
 		</div>
 	)
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
     font-medium transition-all disabled:pointer-events-none disabled:opacity-50
     [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0
     outline-none focus-visible:ring-3 focus-visible:ring-primary/50
-    aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 cursor-pointer`,
+    aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 cursor-pointer active:scale-98 transition-all duration-100`,
 	{
 		variants: {
 			variant: {
